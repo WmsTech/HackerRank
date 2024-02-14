d12bdd52a7b72918f08688e6ec6d4d23fa47113b
WashingtonMSantos <wms.d3v@gmail.com>
2024-02-13 23:19:15 -0400

    .

---
+++ counting_sort.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+#    frequency_array = [0] * (max(arr) + 1)
+    frequency_array = [0] * 100  # Challenge requiried a array with 100 item
+    for number in arr:
+        frequency_array[number] += 1
+    
+    return frequency_array
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
